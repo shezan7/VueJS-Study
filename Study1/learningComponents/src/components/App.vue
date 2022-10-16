45fa698aa956cac1731d6e16674480024e2f1f7e
shezan7 <bsse1023@iit.du.ac.bd>
2022-10-16 08:17:48 -0700

    add article.vue for props

--- Study1/learningComponents/src/components/App.vue
+++ Study1/learningComponents/src/components/App.vue
@@ -3,16 +3,19 @@
     <Greet_Things name="Shezan" channelName="IIT" />
     <Greet_Things name="Mahmud" channelName="IIT" />
     <Greet_Things :name="name" :channelName="channel" />
+    <Article_Things title="Article Title" :likes="50" :isPublished="true" />
   </div>
 </template>
 
 <script>
 import Greet_Things from './Greet.vue'
+import Article_Things from './Article.vue'
 export default {
   name: 'App',
   components: {
-    Greet_Things
-  },
+    Greet_Things,
+    Article_Things
+},
   data() {
     return {
       name: 'Shezan',
---
+++ Study1/learningComponents/src/components/Article.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h2>Article Content</h2>
+        <h2>{{title}}</h2>
+        <h2>Likes - {{likes}}</h2>
+        <h2>Published - {{isPublished ? 'yes' : 'no'}}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+       name: 'Article_Things',
+       props: {
+        title: {
+            type: String,
+            required: true,
+            // default: 'Article default title'
+        },
+        likes: Number,
+        isPublished: Boolean
+       }
+    }
+</script>
+
+<style scoped>
+
+</style>
